edef51b058d53d6222671f44465c4fdcf2c12478
Dr Jessica Muirhead <github@jessica.digital>
2024-05-24 19:00:45 +0100

    Flutter Bootcamp: Lesson 67

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text(
+          'Ask Me Anything',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blue,
+      body:Container(),
+    );
+  }
+}
